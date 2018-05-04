db2ece8759fc74cb0e221614d1527807d05e7ab8
nono-the-creator <noamambar@gmail.com>
2018-05-04 13:11:20 +0300

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/school_project.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/school_project.dir/main.c.o"
+  "school_project.pdb"
+  "school_project"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/school_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ mainhadder.h
@@ -0,0 +1,46 @@
+//
+// Created by noam ambar on 04/05/2018.
+//
+
+#ifndef SCHOOL_PROJECT_MAINHADDER_H
+#define SCHOOL_PROJECT_MAINHADDER_H
+#include<stdbool.h>
+#include <stdio.h>
+struct date{
+    short int day;
+    short int month;
+    short int year;
+};
+
+struct apart{
+    unsigned int code;
+    char *addr;
+    int price;
+    short int rooms;
+    struct date date_of_entrance;
+    struct date date_stamp;
+};
+//shell
+struct commend_list_node{
+    char* commend;
+    struct commend_list_node* next;
+};
+struct commend_list{
+    struct commend_list_node* head;
+    struct commend_list_node* tail;
+};
+char **GetString(char *);
+void interpet(char **);
+
+
+//db queries
+void print_aprt(int maxPrice, int maxRooms, int minRooms, struct date maxDate, bool flag);
+/*
+ *
+ * input: max price, rooms range and the latest date to enter the apartment.
+ * -1 means ignoring the requirement,
+ * output: prints all the apartment that suits the requirements.
+ */
+
+
+#endif //SCHOOL_PROJECT_MAINHADDER_H
