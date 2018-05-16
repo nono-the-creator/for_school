c22726263afae12ed9303843bd5b68a9e65316cd
Binyamin Darshan Hcohen <binyaminda@mta.ac.il>
2018-05-16 17:30:36 +0300

    Get a basic main going

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include "mainhadder.h"
 
 int main() {
+
+	struct apart_list aprtlst;
+	struct commend_list cmdlst;
+	char *command;
+	char **commend_array;
+	aprtlst.head = aprtlst.tail = NULL; //remove
+	cmdlst.head = cmdlst.tail = NULL; //remove
+	commend_array = malloc(sizeof(char *)* 7);
+	while(!strcmp(command, "exit"))
+	{
+		command = gets();
+		interpert(command, aprtlst, commend_array, &cmdlst);
+
+
+	}
+
+
 
 
     return 0;
