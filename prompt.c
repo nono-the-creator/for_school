d35075646e52fd6cba79c2499de167fdd7e96b56
bennydarshan <bennydarshan@gmail.com>
2018-05-18 09:50:43 +0300

    Fixed get and changed add to take a pointer for lst

--- prompt.c
+++ prompt.c
@@ -1,31 +1,29 @@
 #include "mainhadder.h"
 
-void interpert_add(char *str, struct apart_list lst)
+void interpert_add(char *str, struct apart_list *lst)
 {
 
 	char *addr;
 	int price,rooms;
 	struct date dt;
-	printf(KRED "Got to add\n" KNRM);
-	printf(KRED "entered string:%s\n" KNRM, str);
 	//strtok(str, "\"");
 	addr = strtok(str, "\"");
-	printf(KRED "addr: %s\n" KNRM, addr);
 	price = atoi(strtok(NULL, " "));
-	printf(KRED "price: %d\n" KNRM, price);
 	rooms = atoi(strtok(NULL, " "));
-	printf(KRED "rooms: %d\n" KNRM, rooms);
 	dt.day = (short)atoi(strtok(NULL, " "));
 	dt.month = (short)atoi(strtok(NULL, " "));
 	dt.year = (short)atoi(strtok(NULL, " "));
 	//call add with the paramters;
-	add_apart_by_price(&lst, addr, price, rooms, dt);
+	printf(KRED "created: addr: %s, price: %d, rooms: %d, day: %d, month: %d, year: %d" KNRM "\n", addr, price, rooms, dt.day, dt.month, dt.year);
+	add_apart_by_price(lst, addr, price, rooms, dt);
 }
 
 void interpert_get(char *str, struct apart_list lst)
 {
-	int max_price, max_rooms, min_rooms;
+	int max_price=-1, max_rooms=-1, min_rooms=-1;
 	struct date min_date;
+	min_date.day = -1;
+	str[strlen(str)]= ' ';
 	char *p;
 	p = strtok(str, " ");
 	while(p)
@@ -55,13 +53,15 @@ void interpert_get(char *str, struct apart_list lst)
 		p = strtok(NULL, " ");
 	}
 	// call the print function with the proper paramters
+	printf(KRED "Sending print with price: %d, max_rooms: %d, min_rooms: %d" KNRM "\n", max_price, max_rooms, min_rooms);
+	print_by_values(lst, max_price, max_rooms, min_rooms, min_date, true);
 }
-void interpert_buy(char *str, struct apart_list lst)
+void interpert_buy(char *str, struct apart_list *lst)
 {
 	//FIXME: might not work because lst is sent by value.
 	unsigned int code;
 	code = atoi(strtok(str, " "));
-	buy_apartment(&lst, code);
+	buy_apartment(lst, code);
 	//call buy with the parameter;
 }
 void interpert_get_enter(char *str, struct apart_list lst)
@@ -82,7 +82,7 @@ void interpert_del_enter(char *str, struct apart_list lst)
 	delte_apart_in_env(&lst, days_env);
 }
 
-void interpert(char *str, struct apart_list lst, char **recent_commends_array, struct commend_list *clst)
+void interpert(char *str, struct apart_list *lst, char **recent_commends_array, struct commend_list *clst)
 {
 	char *command;
 	printf(KRED "Command entered: %s\n" KNRM, str); //DEBUG
@@ -95,7 +95,7 @@ void interpert(char *str, struct apart_list lst, char **recent_commends_array, s
 	printf(KRED "Current string %s\n" KNRM, command);
 	if(strcmp(command, "get-an-apt") == 0)
 	{
-		interpert_get(str + strlen(command) + 1, lst);
+		interpert_get(str + strlen(command) + 1, *lst);
 	}
 	else if(strcmp(command, "add-an-apt") == 0)
 	{
@@ -107,11 +107,11 @@ void interpert(char *str, struct apart_list lst, char **recent_commends_array, s
 	}
 	else if(strcmp(command, "get-an-apt-Enter") == 0)
 	{
-		interpert_get_enter(str+strlen(command) + 1, lst);
+		interpert_get_enter(str+strlen(command) + 1, *lst);
 	}
 	else if(strcmp(command, "delete-an-apt-Enter") == 0)
 	{
-		interpert_del_enter(str+strlen(command) + 1, lst);
+		interpert_del_enter(str+strlen(command) + 1, *lst);
 	}
 	else
 	{
