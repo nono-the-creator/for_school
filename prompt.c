4620ea277c57e322277d0db6836ce61ae6b2736d
Binyamin Darshan Hcohen <binyaminda@mta.ac.il>
2018-06-01 14:08:06 +0300

    Changed year=-1

--- prompt.c
+++ prompt.c
@@ -13,7 +13,7 @@ void interpert_add(char *str, struct apart_list *lst)
 	dt.day = (short)atoi(strtok(NULL, " "));
 	dt.month = (short)atoi(strtok(NULL, " "));
 	dt.year = (short)atoi(strtok(NULL, " "));
-	st.day = -1;
+	st.year = -1;
 	//call add with the paramters;
 	printf(KRED "created: addr: %s, price: %d, rooms: %d, day: %d, month: %d, year: %d" KNRM "\n", addr, price, rooms, dt.day, dt.month, dt.year);
 	add_apart_by_price(lst, addr, price, rooms, dt, -1, st);
