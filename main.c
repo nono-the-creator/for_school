3de08da251a34841e911f3a2bcb60b27da5423cd
nono-the-creator <noamambar@gmail.com>
2018-06-06 17:12:13 +0300

    push the but

--- main.c
+++ main.c
@@ -8,16 +8,11 @@ int main() {
 	char command[128];
 	char **commend_array;
 	commends_amount = 0;
-	aprtlst.head = aprtlst.tail = NULL; //remove
 	aprtlst = read_apartments();
     commend_array = malloc(sizeof(char *)* 7);
     initialize_commends_to_null(&cmdlst,commend_array);
     read_commends_from_file(&cmdlst,commend_array);
     printf(">> ");
-    history(cmdlst,commend_array);
-    reverse(&cmdlst.head);
-    printf("\nafter reverse:\n");
-    history(cmdlst,commend_array);
     gets(command);
     while(strcmp(command, "exit"))
 	{
